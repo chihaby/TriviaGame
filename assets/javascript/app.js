4f5e98bfe6f8f76a0756d920695baa6b3f27d6fb
Radouane <38984550+Radouane@users.noreply.github.com>
2019-01-14 13:58:07 -0800

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,46 @@
 var correctAnswer = 0;
 var incorrectAnswer = 0;
 var unAnswer = 0;
+var questions = [{
+    question: "In Aladdin, what is the name of Jasmine's pet tiger?",
+    choices: ["Rajah", "Bo", "Iago", "Jack"],
+    images: ["../images/Rajah.gif"],
+    correct: 0
+}, {
+    question: "In Peter Pan, Captain Hook had a hook on which part of his     body?",
+    choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
+    correct: 1
+
+}, {
+    question: "In the Lion King, where does Mufasa and his family live?",
+    choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
+    correct: 3
+
+}, {
+    question: "In Beauty and the Beast, how many eggs does Gaston eat for    breakfast?",
+    choices: ["2 Dozen", "5 Dozen", "5000", "0"],
+    correct: 1
+
+}, {
+    question: "In Alice in Wonderland, what is the name of Alice’s kitten?",
+    choices: ["Dinah", "Sammie", "Kat", "Luna"],
+    correct: 0
+
+}, {
+    question: "After being on earth, where did Hercules first meet his   father Zeus?",
+    choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
+    correct: 2
+
+}, {
+    question: "During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
+    choices: ["Yellow", "Blue", "Gold", "White"],
+    correct: 2
+
+}, {
+    question: "In Bambi, what word does the owl use to describe falling in love?",
+    choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
+    correct: 3
+}];
 
 function countDown(n) {
     const int = setInterval(function () {
@@ -10,3 +50,7 @@ function countDown(n) {
 }
 countDown(30);
 
+var ask = function() {
+    
+}
+$('#question-div').text();
