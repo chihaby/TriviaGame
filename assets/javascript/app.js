dc90483452c37eeb7ff2d2a7ebb90743df95f115
Radouane <38984550+Radouane@users.noreply.github.com>
2019-01-24 08:33:07 -0800

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,38 +2,42 @@ var timeFrame = 5;
 var counter = 0;
 var correct = 0;
 var incorrect = 0;
-var n = 0;
-
 
 var questionArr = ["What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
 var answerArr = [["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
-var imageArr = ["<img class='center-block img-right' src='img/australia.png'>", "<img class='center-block img-right' src='img/liberia.png'>", "<img class='center-block img-right' src='img/taiwan.png'>", "<img class='center-block img-right' src='img/japan.png'>", "<img class='center-block img-right' src='img/china.png'>", "<img class='center-block img-right' src='img/turkey.png'>", "<img class='center-block img-right' src='img/colombia.png'>", "<img class='center-block img-right' src='img/india.png'>"];
 var correctArr = ["Canberra", "Monrovia", "Taipei", "Tokyo", "Beijing", "Ankara", "Bogota", "New Delhi"];
 
-function start(n) {    
-    var n = counter++;
+function start() {    
+  
+    var n = counter;
     $('#question').text(questionArr[n]);
     $('#answerA').text(answerArr[n][0]);
     $('#answerB').text(answerArr[n][1]);
     $('#answerC').text(answerArr[n][2]);
     $('#answerD').text(answerArr[n][3]);
-    $("button").click(function(){   
-             
-            if( $(this).text() === correctArr[n]){
-                correct++;
-                $('.result').text('CORRECT');             
-                $('#progress').text('CORRECT: ' + correct + '/ ' + questionArr.length); 
-                          
-            }
-            else {
-                incorrect++;
-                $('.result').text('Correct answer is: ' + correctArr[n]);
-                $('#progress').text('WRONG: ' + incorrect + '   ------  CORRECT: ' + correct);                
-                
-            }   
-        start();
+
+    $("button").click(function(){  
+        
+        $('#question').text(questionArr[n]);
+        $('#answerA').text(answerArr[n][0]);
+        $('#answerB').text(answerArr[n][1]);
+        $('#answerC').text(answerArr[n][2]);
+        $('#answerD').text(answerArr[n][3]);
+        
+
+        if( $(this).text() === correctArr[n]){
+            correct++;
+            $('.result').text('CORRECT');             
+            $('#progress').text('CORRECT: '+ correct)                      
+        }
+        if( $(this).text() !== correctArr[n]) {
+            incorrect++;
+            $('.result').text('Correct answer is: ' + correctArr[n]);
+            $('#progress').text('WRONG: '+ incorrect)                            
+        }  
+    n++;
     });  
-  
+
 }
 
 function countDown(){
@@ -50,14 +54,9 @@ function countDown(){
     }    
 }
 
-
-
 start();
-
 countDown();
 
-
-//setTimeout(game.nextQuestion, 3 * 1000);
 
 
 
